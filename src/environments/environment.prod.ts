0239ebc5d39821017026a63ba61a93e4e068ac6a
schneiderstag <brunopiasso@hotmail.com>
2020-07-07 23:36:41 +0100

    First commit - Up to lesson 8 - Drop Down Menu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAjof3K5Yn4P5JCmjEDANPSOmaAxch6liE',
+    authDomain: 'oshop-3d359.firebaseapp.com',
+    databaseURL: 'https://oshop-3d359.firebaseio.com',
+    projectId: 'oshop-3d359',
+    storageBucket: 'oshop-3d359.appspot.com',
+    messagingSenderId: '415008692692',
+    appId: '1:415008692692:web:47acff8d301672dab55e41',
+    measurementId: 'G-0L6THWFKF7',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAjof3K5Yn4P5JCmjEDANPSOmaAxch6liE',
+    authDomain: 'oshop-3d359.firebaseapp.com',
+    databaseURL: 'https://oshop-3d359.firebaseio.com',
+    projectId: 'oshop-3d359',
+    storageBucket: 'oshop-3d359.appspot.com',
+    messagingSenderId: '415008692692',
+    appId: '1:415008692692:web:47acff8d301672dab55e41',
+    measurementId: 'G-0L6THWFKF7',
+  },
 };
 
 /*
