b49c12d567e5876d966d436f083fd12b8d791ff1
schneiderstag <brunopiasso@hotmail.com>
2020-09-19 11:00:48 +0100

    Dropdown menu fix pt.1

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs/operators';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
@@ -9,7 +10,17 @@ export class CategoryService {
   constructor(private db: AngularFireDatabase) { }
 
   getCategories() {
-    return this.db.list('/categories', c => c.orderByChild('name')).valueChanges();
+    return this.db.list('/categories', c => c.orderByChild('name'))
+    .snapshotChanges()
+    .pipe(map(items => {
+      return items.map(a => {
+        const name = a.payload.val();
+        const key = a.payload.key;
+        return {key, name}; // or {key, ...data} in case data is Obj
+      });
+    }));
+
+    //return this.db.list('/categories', c => c.orderByChild('name')).valueChanges();
     //return this.db.list('/categories').valueChanges();
     //return this.db.list('/categories', c => c.orderByChild('name')).snapshotChanges();
     //return this.db.list('/categories').snapshotChanges();
