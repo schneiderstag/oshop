32478a24a7a2531bc19609f2f8ccf0c572657a18
schneiderstag <brunopiasso@hotmail.com>
2020-09-21 22:14:45 +0100

    Dropdown Menu Working

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -10,19 +10,9 @@ export class CategoryService {
   constructor(private db: AngularFireDatabase) { }
 
   getCategories() {
-    return this.db.list('/categories', c => c.orderByChild('name'))
-    .snapshotChanges()
-    .pipe(map(items => {
-      return items.map(a => {
-        const name = a.payload.val();
-        const key = a.payload.key;
-        return {key, name}; // or {key, ...data} in case data is Obj
-      });
-    }));
-
     //return this.db.list('/categories', c => c.orderByChild('name')).valueChanges();
     //return this.db.list('/categories').valueChanges();
-    //return this.db.list('/categories', c => c.orderByChild('name')).snapshotChanges();
+    return this.db.list('/categories', c => c.orderByChild('name')).snapshotChanges();
     //return this.db.list('/categories').snapshotChanges();
   }
 }
