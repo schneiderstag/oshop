073f9fbf2e40f66e90f61e4f2a362a806f5faccf
schneiderstag <brunopiasso@hotmail.com>
2020-09-28 17:29:59 +0100

    Added product search field

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,22 @@
+import { Product } from './../../models/product';
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy { 
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
-  constructor(
-    private router: Router,
-    private productService: ProductService) {
-    this.products$ =  this.productService.getAll();
+  constructor(private router: Router, private productService: ProductService) {
+    this.subscription = this.productService.getAll()
+    .subscribe(products => this.filteredProducts = this.products = products);
   }
 
   delete(productId) {
@@ -21,6 +24,15 @@ export class AdminProductsComponent implements OnInit {
 
     this.productService.delete(productId);
     this.router.navigate(['/admin/products']);
+  }
+
+  filter(query: string) {
+    console.log(query);
+    this.filteredProducts = (query) ? this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit(): void {
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from './models/product';
+import { AngularFireDatabase, SnapshotAction } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +15,26 @@ export class ProductService {
     return this.db.list('/products').push(product);
   }
 
-  getAll(){
-    return this.db.list('/products').snapshotChanges();
+  getAll(): Observable<Product[]> {
+    return this.db.list<Product>('/products').snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(a => ({ key: a.key, ...a.payload.val() }))
+        )
+      );
   }
   
-  get(productId){
+  // getAll()  {
+  //   return this.db.list('/products').snapshotChanges();
+  // }
+
+  get(productId: number) {
     //What's the difference between valueChanges() and snapshotChanges()?
     //return this.db.object('/products/' + productId).valueChanges();
     return this.db.object('/products/' + productId).valueChanges().pipe(tap(data => console.log(JSON.stringify(data))));
   }
 
-  update(productId, product) {
+  update(productId: number, product: Product) {
     return this.db.object('/products/' + productId).update(product);
   }
 
